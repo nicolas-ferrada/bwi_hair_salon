c2adbb68fbb0a6d399b85aba72fd13666f9951e7
nicolas-ferrada <nicofrrd@gmail.com>
2023-11-05 16:37:27 -0300

    feat firebase initial configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:bwi_hair_salon/domain/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/pages/home/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
