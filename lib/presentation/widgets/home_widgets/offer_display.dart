021fe3fc2dcdeab0108a5c17c845abdf568dc08a
nicolas-ferrada <nicofrrd@gmail.com>
2023-11-09 02:04:13 -0300

    feat offer display screen

--- lib/presentation/widgets/home_widgets/offer_display.dart
+++ lib/presentation/widgets/home_widgets/offer_display.dart
@@ -11,16 +11,71 @@ class OfferDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 200,
+      child: PageView.builder(
+        physics: const BouncingScrollPhysics(
+          decelerationRate: ScrollDecelerationRate.fast,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: offerList.length,
+        itemBuilder: (context, index) {
+          return imageContainer(
+            offer: offerList[index],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget imageContainer({required Offer offer}) {
     return AspectRatio(
       aspectRatio: 16 / 9,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.red,
+          border: Border.all(color: Colors.black),
           borderRadius: BorderRadius.circular(15.0),
-          // image:
-          //     DecorationImage(image: const NetworkImage(''  ), fit: BoxFit.cover),
+          image: DecorationImage(
+            image: NetworkImage(offer.image),
+            fit: BoxFit.cover,
+          ),
         ),
-        child: Text(offerList[0].description),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                offer.offer,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.black,
+                ),
+              ),
+              Text(
+                offer.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                offer.description,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
